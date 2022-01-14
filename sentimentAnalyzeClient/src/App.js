1b722eb88107086eab9ffd094e47d896c8d9fea7
chloepomeroy <pomerroyc@outlook.com>
2022-01-14 17:20:50 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -75,6 +75,7 @@ class App extends React.Component {
   render() {
     return (  
       <div className="App">
+          <head><title>Sentiment Analyzer</title></head>
       <button className="btn btn-info" onClick={()=>{this.renderOutput('text')}}>Text</button>
         <button className="btn btn-dark"  onClick={()=>{this.renderOutput('url')}}>URL</button>
         <br/><br/>
